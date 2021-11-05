c0fab6652b8c33dab46401df7ece1744b4781c95
Jean-Sébastien Gonsette <jeansebastien.gonsette@gmail.com>
2021-11-05 12:30:13 +0100

    Test with new penguin design

---
+++ contest/game/pingouins2/language.js
@@ -0,0 +1,14 @@
+
+var languages = {
+    "en": {
+        blue_wins: "CARDA-TM wins !",
+        green_wins: "Congrat, you win !",
+        drawn: "You were so close ..."
+    },
+
+    "fr": {
+        blue_wins: "CARDA-TM remporte la partie !",
+        green_wins: "Félicitations, vous avez gagné !",
+        drawn: "C'était si proche ..."
+    }
+}
